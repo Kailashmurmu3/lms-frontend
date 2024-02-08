667f35108afdd217604cbef1918d20ad4bf7dc29
KAILASH MURMU <kailashmurmu0303@gmail.com>
2024-02-08 07:22:56 +0530

    Added about us page

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,9 +1,19 @@
 import { FiMenu } from 'react-icons/fi';
 import { AiFillCloseCircle } from "react-icons/ai";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Footer from '../Components/Footer';
+import { useDispatch, useSelector } from 'react-redux';
 
 function HomeLayout({ children }) {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    // For checking if user is logged in
+    const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+    // For displaying the options acc to role
+    const role = useSelector((state) => state?.auth?.role);
 
     function changeWidth() {
         const drawerSide = document.getElementsByClassName("drawer-side");
@@ -18,6 +28,15 @@ function HomeLayout({ children }) {
 
         const drawerSide = document.getElementsByClassName("drawer-side");
         drawerSide[0].style.width = '0';
+    }
+
+    function handleLogout(e){
+        e.preventDefault();
+
+        // const res = await dispatch(logout())
+        
+        // if(res?.payload?.succes)
+        navigate('/');
     }
 
     return (
@@ -35,7 +54,7 @@ function HomeLayout({ children }) {
                 </div>
                 <div className='drawer-side w-0'>
                     <label htmlFor='my-drawer' className='drawer-overlay'></label>
-                    <ul className="menu p-4 w-48 sm:w-80 bg-base-200 text-base-content relative">
+                    <ul className="menu p-4 w-48 h-[100%] sm:w-80 bg-base-200 text-base-content relative">
                         <li className='w-fit absolute right-2 z-50'>
                             <button onClick={hideDrawer}>
                                 <AiFillCloseCircle size={24} />
@@ -44,6 +63,13 @@ function HomeLayout({ children }) {
                         <li>
                             <Link to="/">Home</Link>
                         </li>
+
+                        {/* For User are LoggedIn */}
+                        {isLoggedIn && role == 'ADMIN' && (
+                            <li>
+                                <Link to='/admin/dashboard'>Admin DashBoard</Link>
+                            </li>
+                        )}
                         <li>
                             <Link to="/courses">All Courses</Link>
                         </li>
@@ -53,6 +79,34 @@ function HomeLayout({ children }) {
                         <li>
                             <Link to="/about">About Us</Link>
                         </li>
+
+                        {/* For User are not LogIn */}
+
+                        {isLoggedIn && (
+                            <li className='absolute bottom-4 w-[90%]'>
+                                <div className="w-full flex items-center justify-center">
+                                    <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link to='/login'>Login</Link>
+                                    </button>
+                                    <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link to='/singup'>Singup</Link>
+                                    </button>
+                                </div>
+                            </li>
+                        )}
+                        {isLoggedIn && (
+                            <li className='absolute bottom-4 w-[90%]'>
+                                <div className="w-full flex items-center justify-center">
+                                    <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link to='/user/profile'>Profile</Link>
+                                    </button>
+                                    <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link onClick={handleLogout}>Logout</Link>
+                                    </button>
+                                </div>
+                            </li>
+                        )}
+                        
                     </ul>
                 </div>
             </div>
